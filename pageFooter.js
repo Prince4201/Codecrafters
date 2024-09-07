ef0a8f00f962c956bdac165b700354c77cf20872
Prince4201 <pj1308109@gmail.com>
2024-09-08 04:42:49 +0530

    Add files via upload

---
+++ pageFooter.js
@@ -0,0 +1,23 @@
+Vue.component('page-footer', {
+    props: ["alt"],
+    template: `<div style="position:relative;bottom:0;width:100%;left:0">
+<div style="height:7vh"></div>
+<footer class="jumbotron  mb-0 py-3 pt-4 footer" style="background-color: var(--grey4)">
+
+<div class="container">
+  
+<div class="row ">
+  <div class="col">
+  <p class=""><img height="40px" class="mr-3" src="/faviconGrey.png"/><u><a class="text-dark" href="/https://www.mathacademy.com/faq/c" }} - all rights reserved.</a></u></p> 
+</div>
+<div class="col text-right">
+ <p><a href="https://discord.gg/NPbfRt4wNM" tabindex="1" target="_blank" class="btn text-dark py-0  px-1"><img height="40px"  src="/icons/discordlogo.png"/> <u>Join us on Discord!</u></a></p>
+</div>
+</div>
+</div>
+</footer>
+</div>`});
+
+var pageFoot = new Vue({
+    el:".page-footer"
+});
---
+++ search.js
@@ -0,0 +1,46 @@
+var sdata;
+function showR(sdata){
+var title;
+var desc ;
+var link ;
+var card ;
+const cont =  document.getElementById("sres");
+const urlParams = new URLSearchParams(window.location.search);
+const myParam = urlParams.get('q');
+if (myParam==null){
+  cont.innerHTML = `<h1>Enter your query in the search box above.</h1>`;
+  return null
+}
+cont.innerHTML += `<h1>Search: ${myParam}</h1>`;
+var results = sdata.items
+var nResults = sdata.searchInformation.totalResults;
+if (nResults==0){
+document.getElementById("spin").style.display="none";
+cont.innerHTML += `<p class="mt-2">There are no results for your query.</p>`;
+return null;}
+console.log(nResults.length);
+results.forEach(function (currentValue){
+title = currentValue.htmlTitle;
+desc = currentValue.snippet;
+link = currentValue.formattedUrl;
+link = link.replace("socrathematics.github","ymath");
+card = `<div class="card pop my-3 no-def">
+<div class="card-body">
+    <h5 class="card-title">${title}</h5>
+    <p class="card-text">${desc}</p>
+      <a href="${link}" class="float-right btn btn-lg text-light btn-success stretched-link"  style="min-width:10rem">Visit</a>
+  </div>
+</div>`;
+cont.innerHTML += card;});
+document.getElementById("spin").style.display="none";
+}
+
+function getData(){
+const urlParams = new URLSearchParams(window.location.search);
+const myParam = urlParams.get('q');
+fetch(`https://www.googleapis.com/customsearch/v1?key=AIzaSyA6bA3PlHiFGHB1CIWobuBUsEa9IQ7AL3I&cx=016043130169791063080:mbkue25xulm&q=${myParam}`)
+.then(response => {return response.json()})
+.then(data => {console.log(data);showR(data)})
+return sdata;
+
+}
